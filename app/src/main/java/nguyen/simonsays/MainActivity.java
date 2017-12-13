2f475f003138632a2ba46386e0045463c0da7767
eatdabanunu <duynguyen.xz@gmail.com>
2017-12-13 12:07:22 -0800

    Update: 12/13/2017

--- app/src/main/java/nguyen/simonsays/MainActivity.java
+++ app/src/main/java/nguyen/simonsays/MainActivity.java
@@ -1,13 +1,40 @@
 package nguyen.simonsays;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    //define variables
+    private ImageButton playButton, highscoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (ImageButton) findViewById(R.id.playButton);
+        highscoreButton = (ImageButton) findViewById(R.id.highscoreButton);
+
+        playButton.setOnClickListener(this);
+        highscoreButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.playButton:
+                Intent playIntent = new Intent(this, PlayActivity.class);
+                startActivity(playIntent);
+                break;
+            case R.id.highscoreButton:
+                Intent highscoreIntent = new Intent(this, HighscoreActivity.class);
+                startActivity(highscoreIntent);
+                break;
+        }
     }
 }

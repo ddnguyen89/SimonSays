c0e90a3ca6bba0cc7df8065a7ad3e460b74533af
eatdabanunu <duynguyen.xz@gmail.com>
2017-12-14 13:10:28 -0800

    Update: 12/14/2017

--- app/src/main/java/nguyen/simonsays/PlayActivity.java
+++ app/src/main/java/nguyen/simonsays/PlayActivity.java
@@ -1,14 +1,18 @@
 package nguyen.simonsays;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class PlayActivity extends AppCompatActivity implements OnClickListener{
@@ -18,9 +22,12 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener{
     private Button greenButton, redButton, blueButton, yellowButton;
     private ImageButton quitButton;
 
-    private String[] simonSays;
-    private String[] userSays;
+    private List<Integer> simonSays = new ArrayList<Integer>();
+    private List<Integer> userSays = new ArrayList<Integer>();
 
+    private final int FLASH_DURATION = 1200;
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,18 +50,45 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener{
         yellowButton.setOnClickListener(this);
 
         quitButton.setOnClickListener(this);
+
+        playGame();
+
+
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.greenButton:
+                userSays.add(1);
+                checked();
+
+
+                Log.d("green", userSays.toString());
+                Log.d("simon", simonSays.toString());
                 break;
             case R.id.redButton:
+                userSays.add(2);
+                checked();
+
+
+                Log.d("red", userSays.toString());
+                Log.d("simon", simonSays.toString());
                 break;
             case R.id.blueButton:
+                userSays.add(3);
+                checked();
+
+                Log.d("blue", userSays.toString());
+                Log.d("simon", simonSays.toString());
                 break;
             case R.id.yellowButton:
+                userSays.add(4);
+                checked();
+
+                Log.d("yellow", userSays.toString());
+                Log.d("simon", simonSays.toString());
                 break;
             case R.id.quitButton:
                 Intent mainIntent = new Intent(this, MainActivity.class);
@@ -63,13 +97,112 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener{
         }
     }
 
+    //function to match user action with simon
+    public boolean checkMatch(List<Integer> simon, List<Integer> user) {
+        boolean matchStatement = false;
 
-    //function to create a random number from 1-4
-    public int getRandomNum() {
+        for(int i = 0; i < user.size(); i++) {
+            if(simon.get(i).equals(user.get(i))) {
+                matchStatement = false;
+                break;
+            } else {
+                matchStatement = true;
+            }
+        }
+        return matchStatement;
+    }
+
+    //function to create and add random number from 1-4 to simon
+    public void addRandomNumber() {
         final int min = 1;
         final int max = 4;
-        int randomButton = new Random().nextInt((min - max) + 1) + min;
+        int randomNumber = new Random().nextInt((max - min) + 1) + min;
 
-        return randomButton;
+        simonSays.add(randomNumber);
+        Log.d("number", String.valueOf(randomNumber));
+    }
+
+    public void playGame() {
+
+        addRandomNumber();
+        simonFlash();
+
+        Log.d("simon size", String.valueOf(simonSays.size()));
+        Log.d("user size", String.valueOf(userSays.size()));
+    }
+
+    public void checked() {
+        if (checkMatch(simonSays, userSays)) {
+            Intent highschoolIntent = new Intent(this, HighscoreActivity.class);
+            startActivity(highschoolIntent);
+            Log.d("not a match", String.valueOf(checkMatch(simonSays, userSays)));
+        } else {
+            addRandomNumber();
+
+                simonFlash();
+
+            Log.d("match", String.valueOf(checkMatch(simonSays, userSays)));
+        }
+    }
+
+    public void simonFlash() {
+        new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    for (int i = 0; i < simonSays.size(); i++) {
+
+                        if (simonSays.get(i) == 1) {
+                            greenButton.setPressed(true);
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    greenButton.setPressed(false);
+                                    handler.postDelayed(this, 500);
+                                }
+                            }, FLASH_DURATION);
+                            Log.d("number", String.valueOf(simonSays.get(i)));
+
+
+                        }
+
+                        else if (simonSays.get(i) == 2) {
+                            redButton.setPressed(true);
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    redButton.setPressed(false);
+                                }
+                            }, FLASH_DURATION);
+                            Log.d("number", String.valueOf(simonSays.get(i)));
+
+                        }
+
+                        else if (simonSays.get(i) == 3) {
+                            blueButton.setPressed(true);
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    blueButton.setPressed(false);
+                                }
+                            }, FLASH_DURATION);
+                            Log.d("number", String.valueOf(simonSays.get(i)));
+
+                        }
+
+                        else if (simonSays.get(i) == 4) {
+                            yellowButton.setPressed(true);
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    yellowButton.setPressed(false);
+                                }
+                            }, FLASH_DURATION);
+                            Log.d("number", String.valueOf(simonSays.get(i)));
+
+                        }
+                    }
+                }
+        }, 1000);
     }
 }
